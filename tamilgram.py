babe5df601ea68d7a5c232c519a8f5169f4717a0
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-11-16 18:06:45 +0530

    Update tamilgram.py

--- tamilgram.py
+++ tamilgram.py
@@ -1,1 +1,14 @@
+### tamilgram ####
 
+from shortzy import shortzy
+
+##
+
+SHORTENER_WEBSITE_API = os.environ.get("SHORTENER_WEBSITE_API")
+SHORTENER_WEBSITE = os.environ.get("SHORTENER_WEBSITE")
+
+##
+async def get_shortlink(url):
+    if SHORTENER_WEBSITE_API and SHORTENER_WEBSITE:
+        shortzy = Shortzy(SHORTENER_WEBSITE_API, SHORTENER_WEBSITE)
+        return await shortzy.convert(url)
